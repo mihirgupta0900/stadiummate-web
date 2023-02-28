2b9b2dc9453ae43392e6753f9391f2a0e5d3d0f3
Mihir Gupta <mihirgupta0900@gmail.com>
2023-02-28 16:36:08 +0530

    fix(engagement): rename and add to root router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
 import { watchPartyRouter } from "./routers/watchPartyRouter";
+import { engagementRouter } from "./routers/engagementRouter";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { watchPartyRouter } from "./routers/watchPartyRouter";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   watchParty: watchPartyRouter,
+  engagement: engagementRouter,
 });
 
 // export type definition of API
