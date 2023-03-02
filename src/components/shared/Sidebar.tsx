9e88ee2044971a2fc71da506316723e037180abd
Mihir Gupta <mihirgupta0900@gmail.com>
2023-03-03 05:04:57 +0530

    fix: add link for voice

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar: FC = () => {
   const nonMatchModeRoutes = [
     ["Feed", "/update", "/icons/cricket.svg"],
     ["experience", "/watchparty", "/icons/experience.svg"],
-    ["voice", "/voice", "/icons/mic.svg"],
+    ["voice", "https://voice-stadiummate.vercel.app/", "/icons/mic.svg"],
     // ["profile", "/profile", "/icons/profile.svg"],
   ] as const;
 
